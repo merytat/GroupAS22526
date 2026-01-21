60cdf6f548528e965ff66022965e637de1e4713f
Mery Tellez <76423201+merytat@users.noreply.github.com>
2026-01-21 09:19:43 +0300

    Jan 21 Variables

--- Unit1/HelloWorld.py
+++ Unit1/HelloWorld.py
@@ -1,1 +1,7 @@
-print("Hello World")
+# Program to print Hello World
+print("H")  # This is a line for printing
+print("Hello", 2, "World")
+print("Hello " + "2 "  + "World")
+print ("something", end=' ')
+print("in front of something")
+
---
+++ Unit1/Youdo9.py
@@ -0,0 +1,7 @@
+greeting = "Hi There"
+num = 3
+value = num + 1
+
+print(greeting)
+print(num * greeting)
+print(value + num)
